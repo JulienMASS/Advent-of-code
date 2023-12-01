c81fc1371fe6b33ede0a194c458acb7bc52f3f4e
Julien Vannier <jvannier@meilleurtaux.com>
2023-12-01 13:58:50 +0100

    Ajoutez des fichiers projet.

---
+++ Day1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Advent_of_code_2023
+{
+    internal class Day1
+    {
+        public int SolvePart1(string[] input)
+        {
+            int sum = 0;
+            foreach (string line in input)
+            {
+                char[] numbers = new char[2];
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (numbers[0] == default && char.IsDigit(line[i]))
+                        numbers[0] = line[i];
+                    if (numbers[1] == default && char.IsDigit(line[line.Length - 1 - i]))
+                        numbers[1] = line[line.Length - 1 - i];
+                    if (numbers[0] != default && numbers[1] != default)
+                        break;
+                }
+                sum += int.Parse(new string(numbers));
+            }
+            return sum;
+        }
+
+        public int SolvePart2Test(string[] args)
+        {
+            Dictionary<string, char> stringDigits = new Dictionary<string, char>()
+            {
+                {"one",'1'},
+                {"two",'2'},
+                {"three",'3'},
+                {"four",'4'},
+                {"five",'5'},
+                {"six",'6'},
+                {"seven",'7'},
+                {"eight",'8'},
+                {"nine",'9'},
+            };
+            String[] newInput = new string[args.Length];   
+            for (int i = 0 ; i < args.Length ; i++)
+            {
+            }
+            return SolvePart1(newInput);
+        }
+
+        public int SolvePart2(string[] input)
+        {
+            int sum = 0;
+            Dictionary<string, char> stringDigits = new Dictionary<string, char>()
+            {
+                {"one",'1'},
+                {"two",'2'},
+                {"three",'3'},
+                {"four",'4'},
+                {"five",'5'},
+                {"six",'6'},
+                {"seven",'7'},
+                {"eight",'8'},
+                {"nine",'9'},
+            };
+            foreach (string line in input)
+            {
+                char[] numbers = new char[2];
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (numbers[0] == default) {
+                        if(char.IsDigit(line[i]))
+                            numbers[0] = line[i];
+                        else
+                        {
+                            for (int j = 3; j <= 5; j++)
+                            {
+                                string nextChar = line[i..(Math.Min(i + j, line.Length - 1))];
+                                if (stringDigits.ContainsKey(nextChar))
+                                {
+                                    numbers[0] = stringDigits[nextChar];
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                    if (numbers[1] == default)
+                    {
+                        if (char.IsDigit(line[line.Length - 1 - i]))
+                            numbers[1] = line[line.Length - 1 - i];
+                        else
+                        {
+                            for(int j = 3; j <= 5; j++)
+                            {
+                                string nextChar = line[(Math.Max(0, line.Length - i - j))..(Math.Max(0, line.Length - i))];
+                                if (stringDigits.ContainsKey(nextChar))
+                                {
+                                    numbers[1] = stringDigits[nextChar];
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                    if (numbers[0] != default && numbers[1] != default)
+                        break;
+                }
+                sum += int.Parse(new string(numbers));
+            }
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Advent_of_code_2023;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+
+
+Console.WriteLine("Bienvenue dans cette nouvelle édition de Advent of code 2023");
+do
+{
+    Console.WriteLine("Quel jour voulez-vous lancer ?");
+    int day = int.Parse(Console.ReadLine()!);
+    string className = $"Day{day}";
+    string input = Advent_of_code_2023.Properties.Resources.ResourceManager.GetString($"Day{day}")!;
+    Type? type = Assembly.GetExecutingAssembly().GetType($"Advent_of_code_2023.{className}");
+    if (type == null || string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine($"Le jour {day} n'existe pas, ou n'a pas d'input");
+    }
+    else
+    {
+        string[] lines = input.Split("\r\n");
+        object? instance = Activator.CreateInstance(type);
+        MethodInfo? method = type.GetMethod("SolvePart1");
+        if (method == null)
+        {
+            Console.WriteLine($"Le jour {day} n'a pas d'implémentation de la partie 1");
+        }
+
+        Stopwatch sw = Stopwatch.StartNew();
+        object? res1 = null;
+            try { res1 = method?.Invoke(instance, new object[] { lines }) ?? "?"; }catch(Exception e) { res1 = e.Message; }
+            
+        string elapsed1 = GetStopWatchString(sw);
+        MethodInfo? method2 = type.GetMethod("SolvePart2");
+        if (method == null)
+        {
+            Console.WriteLine($"Le jour {day} n'a pas d'implémentation de la 2ème solution");
+        }
+        sw.Restart();
+        var res2 = method2?.Invoke(instance, new object[] { lines }) ?? "?";
+        string elapsed2 = GetStopWatchString(sw);
+        sw.Stop();
+        DrawResult($"Résultat de la partie 1 : {res1}, temps : {elapsed1}", $"Résultat de la partie 2 : {res2}, temps : {elapsed2}");
+    }
+}while (true);
+
+//Return millisecond string 
+string GetStopWatchString(Stopwatch sw)
+{
+    var miliseconds = (sw.Elapsed.TotalNanoseconds > 1000000) ? sw.Elapsed.TotalMilliseconds : sw.Elapsed.TotalNanoseconds / 1000000;
+    return  $"{miliseconds:N4} ms";
+}
+
+void DrawResult(string part1,string part2)
+{
+    Console.WriteLine("╔" + new string('═', Math.Max(part1.Length, part2.Length)) + "╗");
+    Console.WriteLine($"║{part1+new string(' ',Math.Max(0,part2.Length-part1.Length))}║");
+    Console.WriteLine("╠" + new string('═', Math.Max(part1.Length, part2.Length)) + "╣");
+    Console.WriteLine($"║{part2 + new string(' ', Math.Max(0,part1.Length - part2.Length))}║");
+    Console.WriteLine("╚" + new string('═', Math.Max(part1.Length, part2.Length)) + "╝");
+}
